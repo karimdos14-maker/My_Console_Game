02362476ea327590bebcc92e8553dcfd5f83442b
karimdos14-maker <karim.dos14@gmail.com>
2026-09-07 21:51:37 +0500

    Add files via upload

--- Guns.h
+++ Guns.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <iostream>
+class Village
+{
+public:
+	const int MAX_BUILDERS   = 6;
+	const int MIN_BUILDERS   = 1;
+	const int MIN_TOWN_HALL  = 1;
+	const int MAX_TOWN_HALL  = 18;
+	const int MAX_FENCE_LVL  = 18;
+	const int MIN_FENCE_LVL  = 1;
+	const int MAX_GUN_LVL = 18;
+	const int MIN_GUN_LVL = 1;
+};
