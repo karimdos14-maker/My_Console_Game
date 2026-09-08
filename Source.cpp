24519c0c2b5abcb76d100a6c499a5cb2059e2ba1
karimdos14-maker <karim.dos14@gmail.com>
2026-09-08 21:26:08 +0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -15,20 +15,41 @@ int SPECIAL_DICTIONARY() {
     std::cout << "================" << std::endl;
     return 0;
 }
-int main() {
 
     
 
-    while (true) {
+int main() {
+
+    char choice1;
+
+    do {
+
         Resources resources;
-        resources.gold = 1000;
-        resources.elixir = 1000;
-        resources.dark_elixir = 0;
         Player player;
         int forCycle = 0;
-        std::string choice; 
-        player.nickName;  
+        std::string choice;
+        player.nickName;    
         std::cout << "***Village***" << std::endl;
+        std::cout << "1 = Resources" << std::endl;
+        std::cout << "2 = Buildings" << std::endl;
+        std::cout << "Q = Quit" << std::endl;
+        std::cin >> choice1;
+        if (choice1 == '1') {
+            resources.gold = 1000;
+            resources.oil = 1000;
+            resources.diamonds = 200;
+            resources.liquid_gems = 0;
+            std::cout << resources.gold << std::endl;
+            std::cout << resources.oil << std::endl;
+            std::cout << resources.diamonds << std::endl;
+            std::cout << resources.liquid_gems << std::endl;
+        }
+        else if (choice1 == '2') {
+
+        }
+        else if (choice1 == 'Q' || choice1 == 'q') {
+            break;
+        }
         Sleep(3000);
         std::cout << "Hello!" << std::endl;
         std::cout << "Create Nickname!" << std::endl;
@@ -46,7 +67,7 @@ int main() {
        
         std::cout << "You village" << '\n' << std::endl;
           
-        break;  
-    } 
+        
+    } while (choice1 != 'Q' && choice1 != 'q');
 
 }   
