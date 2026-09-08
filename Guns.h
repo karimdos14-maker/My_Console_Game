57ab75bdee265ca9c097c346f9e70eaf9061c060
karimdos14-maker <karim.dos14@gmail.com>
2026-09-08 21:55:14 +0500

    Add files via upload

--- Guns.h
+++ Guns.h
@@ -3,10 +3,6 @@
 class Guns
 {
 public:
-	const int MIN_TOWN_HALL  = 1;
-	const int MAX_TOWN_HALL  = 18;
-	const int MAX_FENCE_LVL  = 18;
-	const int MIN_FENCE_LVL  = 1;
 	const int MAX_GUN_LVL = 18;
 	const int MIN_GUN_LVL = 1;
 };
