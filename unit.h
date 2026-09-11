be915c605190964e30bb6bd4381270c9795da0a6
karimdos14-maker <karim.dos14@gmail.com>
2026-09-11 15:04:52 +0500

    Add files via upload

---
+++ unit.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Unit {
+public:
+	struct Witch
+	{
+		char Witch = 'W';
+		int WitchDamage;
+		int WitchHealth;
+	};
+	struct Barbarian
+	{
+		char barbarian = 'B';
+		int BarbarianDamage;
+		int BarbarianHealth;
+	};
+
+	struct Archer {
+		char Archer  = 'B';
+		int ArcherDamage;
+		int ArcherHealth;
+	};
+};
